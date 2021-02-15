8f86063f48c1c4c5f0a1fa34d3c1cdb7eca2fae7
JK <s2007klhwaiommk@gmail.com>
2021-02-15 21:27:31 +0800

    Update conv_blocks.py

--- tf_pose/mobilenet/conv_blocks.py
+++ tf_pose/mobilenet/conv_blocks.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+#modified by lhkwok9
+
 """Convolution blocks for mobilenet."""
 import contextlib
 import functools
